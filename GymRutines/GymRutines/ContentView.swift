c1352f8ce2a347408509507bddbc71ac06e9ab2b
ManuelMorenoCámara <manuel.moreno@babelgroup.com>
2022-10-05 19:43:06 +0200

    initial commit

---
+++ GymRutines/GymRutines/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymRutines
+//
+//  Created by manuel.moreno.local on 5/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymRutines/GymRutines/GymRutinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymRutinesApp.swift
+//  GymRutines
+//
+//  Created by manuel.moreno.local on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GymRutinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
